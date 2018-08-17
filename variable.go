0188150175e187c28a29b0b55bd8b89f7f75e383
Craig Sutherland <cj.sutherland@auckland.ac.nz>
2018-08-17 17:02:40 +1200

    Added variables.

---
+++ variable.go
@@ -0,0 +1,79 @@
+package robolang
+
+import "encoding/json"
+
+// VariableTable defines all the available variables for a block
+type VariableTable struct {
+	Parent    *VariableTable `json:"-"`
+	Variables VariableMap    `json:"variables"`
+}
+
+// NewVariableTable starts a new variable table
+func NewVariableTable(variables ...*VariableDefinition) *VariableTable {
+	table := &VariableTable{
+		Variables: VariableMap{},
+	}
+	if variables != nil {
+		for _, variable := range variables {
+			table.Variables[variable.Name] = variable
+		}
+	}
+	return table
+}
+
+// Get attempts to retrieve a value from table
+func (table *VariableTable) Get(name string) (*VariableDefinition, bool) {
+	value, exists := table.Variables[name]
+	if exists {
+		return value, true
+	}
+	if table.Parent != nil {
+		return table.Parent.Get(name)
+	}
+	return nil, false
+}
+
+// VariableMap is a convience wrapper to simplify the marshalling and unmarshalling of variable definitions
+type VariableMap map[string]*VariableDefinition
+
+// MarshalJSON converts a VariableMap to JSON
+func (fm VariableMap) MarshalJSON() ([]byte, error) {
+	out, pos := make([]VariableDefinition, len(fm)), 0
+	for key, val := range fm {
+		val.Name = key
+		out[pos] = *val
+		pos++
+	}
+	return json.Marshal(out)
+}
+
+// UnmarshalJSON converts JSON to a VariableMap
+func (fm *VariableMap) UnmarshalJSON(data []byte) error {
+	in := []VariableDefinition{}
+	if err := json.Unmarshal(data, &in); err != nil {
+		return err
+	}
+
+	for _, val := range in {
+		out := val
+		(*fm)[val.Name] = &out
+	}
+	return nil
+}
+
+// VariableDefinition defines a variable that holds a value
+type VariableDefinition struct {
+	Name  string  `json:"name"`
+	Value *string `json:"value,omitempty"`
+}
+
+// NewVariable starts a new variable definition
+func NewVariable(name string) *VariableDefinition {
+	return &VariableDefinition{Name: name}
+}
+
+// Set sets the value of the variable
+func (variable *VariableDefinition) Set(value string) *VariableDefinition {
+	variable.Value = &value
+	return variable
+}
---
+++ variable_test.go
@@ -0,0 +1,83 @@
+package robolang
+
+import (
+	"encoding/json"
+	"testing"
+)
+
+func TestVariableMapToJSON(t *testing.T) {
+	fm := VariableMap{}
+	fm["wait"] = NewVariable("wait")
+	fm["hello"] = NewVariable("hello").Set("world")
+	out, err := json.Marshal(fm)
+	if err != nil {
+		t.Errorf("JSON marshal failed: %v", err)
+	}
+
+	expected := "[{\"name\":\"wait\"},{\"name\":\"hello\",\"value\":\"world\"}]"
+	if string(out) != expected {
+		t.Errorf("Unexpected JSON output: expected %s, actual %s", expected, out)
+	}
+}
+
+func TestVariableMapFromJSON(t *testing.T) {
+	fm := VariableMap{}
+	in := "[{\"name\":\"wait\"},{\"name\":\"hello\",\"value\":\"world\"}]"
+	err := json.Unmarshal([]byte(in), &fm)
+	if err != nil {
+		t.Errorf("JSON marshal failed: %v", err)
+	}
+
+	val, ok := fm["hello"]
+	if !ok {
+		t.Errorf("Missing value 'hello', actual is %+v", fm)
+	}
+	expected := "world"
+	if val.Value == nil {
+		t.Errorf("Incorrect value for 'hello': expected %s, actual <nil>", expected)
+	} else if *val.Value != expected {
+		t.Errorf("Incorrect value for 'hello': expected %s, actual %s", expected, *val.Value)
+	}
+}
+
+func TestVariableGet(t *testing.T) {
+	base := NewVariableTable(
+		NewVariable("test"),
+		NewVariable("parent").Set("only"),
+		NewVariable("hello").Set("world"))
+	child := NewVariableTable(
+		NewVariable("hello").Set("there"),
+		NewVariable("me").Set("too"))
+	child.Parent = base
+
+	vals := []string{"world", "there", "only"}
+	tests := []struct {
+		Key      string
+		Exists   bool
+		Expected *string
+		Table    *VariableTable
+	}{
+		{Key: "nothing", Exists: false, Expected: nil, Table: base},
+		{Key: "test", Exists: true, Expected: nil, Table: base},
+		{Key: "hello", Exists: true, Expected: &vals[0], Table: base},
+		{Key: "hello", Exists: true, Expected: &vals[1], Table: child},
+		{Key: "parent", Exists: true, Expected: &vals[2], Table: child},
+		{Key: "neither", Exists: false, Expected: nil, Table: child},
+	}
+	for count, test := range tests {
+		t.Logf("Running test #%d", count+1)
+		value, found := test.Table.Get(test.Key)
+		if test.Exists != found {
+			t.Errorf("Exists condition does not match: expected %v, actual %v", test.Exists, found)
+		}
+		if value != nil {
+			if test.Expected == nil || value.Value == nil {
+				if test.Expected != value.Value {
+					t.Errorf("Value condition does not match: expected %v, actual %v", test.Expected, value.Value)
+				}
+			} else if *test.Expected != *value.Value {
+				t.Errorf("Value condition does not match: expected %s, actual %s", *test.Expected, *value.Value)
+			}
+		}
+	}
+}
